6df6ee7dd87338eb61602822b0e8b31b28f428fd
GitVives <emiel.verheye@student.vives.be>
2026-03-01 14:13:03 +0100

    Labo 14

--- labo_15/opdracht_producten/scripts/code.js
+++ labo_15/opdracht_producten/scripts/code.js
@@ -1,9 +1,29 @@
 const setup = () => {
+    let button = document.getElementById("btn");
 
+    button.addEventListener("click", herbereken);
 }
 
 const herbereken = () => {
-    document.get
+    let prijzen = document.getElementsByClassName("prijs");
+    let btws = document.getElementsByClassName("btw");
+    let subtotalen = document.getElementsByClassName("subtotaal");
+    let inputs = document.getElementsByClassName("input");
+    let output = document.getElementById("totaal");
+    let totaal = 0;
+
+    for (let i = 0; i < prijzen.length; i++) {
+        let prijs = parseInt(prijzen[i].innerText);
+        let btw = parseInt(btws[i].innerText);
+        let aantal = parseInt(inputs[i].value);
+        let result;
+
+        result = (prijs + (prijs * (btw/100))) * aantal;
+        subtotalen[i].innerText = result.toFixed(2);
+        totaal += result;
+    }
+    
+    output.innerText = totaal.toFixed(2);
 }
 
 window.addEventListener("load", setup);
