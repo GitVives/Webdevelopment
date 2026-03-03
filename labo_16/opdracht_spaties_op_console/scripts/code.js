955c6429efedb100787dfdbd0c531d66309bd55f
GitVives <emiel.verheye@student.vives.be>
2026-03-03 16:57:11 +0100

    Labo 16

---
+++ labo_16/opdracht_spaties_op_console/scripts/code.js
@@ -0,0 +1,21 @@
+const setup = () => {
+    let btn = document.getElementById("btn");
+    btn.addEventListener("click", print);
+}
+
+const print = () => {
+    let txt = document.getElementById("txt");
+    let output = "";
+
+    let string = txt.value;
+
+    let result = string.split("").filter((word) => word !== " ");
+
+    for (let i = 0; i < result.length; i++) {
+        output += result[i];
+    }
+
+    console.log(output);
+}
+
+window.addEventListener("load", setup);
